c20f4d4807c50f4f8f0333e6dc38d70330ac0a33
Trần Bá Hậu <bahau2411.work@gmail.com>
2023-07-22 20:22:36 +0700

    [SC]_Create NavHeader component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,8 @@
 import { Link, createSearchParams, useNavigate } from 'react-router-dom';
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import { useContext } from 'react';
 import Popover from '../Popover';
 import { useForm } from 'react-hook-form';
-import authApi from 'src/api/auth.api';
 import AppContext from 'src/context/app.context';
 import { path } from 'src/constant/path';
 import useQueryConfig from 'src/hooks/useQueryConfig';
@@ -14,6 +13,7 @@ import { purchaseSts } from 'src/constant/purchase';
 import purchaseApi from 'src/api/purchase.api';
 import { formatCurrency } from 'src/utils/util';
 import noProduct from 'src/assets/images/noProduct.png';
+import NavHeader from '../NavHeader';
 
 type FormData = Pick<Schema, 'name'>;
 const nameSchema = schema.pick(['name']);
@@ -22,15 +22,7 @@ export default function Header() {
   const queryConfig = useQueryConfig();
   const navigate = useNavigate();
   const { isAuthenticated } = useContext(AppContext);
-  const { setIsAuthenticated, profile } = useContext(AppContext);
   const { register, handleSubmit } = useForm<FormData>({ resolver: yupResolver(nameSchema) });
-
-  const logOutAccountMutation = useMutation({
-    mutationFn: authApi.logoutAccount,
-    onSuccess: () => {
-      setIsAuthenticated(false);
-    }
-  });
 
   const handleSearchProduct = handleSubmit((data) => {
     const config = queryConfig.order
@@ -52,10 +44,6 @@ export default function Header() {
     });
   });
 
-  const onLogOut = () => {
-    logOutAccountMutation.mutate();
-  };
-
   const { data: purchasesInCartData } = useQuery({
     queryKey: ['purchases', { status: purchaseSts.inCart }],
     queryFn: () => purchaseApi.getPurchases({ status: purchaseSts.inCart }),
@@ -67,79 +55,7 @@ export default function Header() {
   return (
     <div className='bg-[linear-gradient(-180deg,#f53d2d,#f63)] pb-5 pt-2 text-white'>
       <div className='container'>
-        <div className='flex justify-end'>
-          <Popover
-            className='mr-3 flex cursor-pointer items-center px-2 hover:text-gray-300'
-            renderPopover={
-              <div className='relative rounded-sm border border-gray-200 bg-white shadow-md'>
-                <div className='flex flex-col py-2 pl-3 pr-28 '>
-                  <button className='px-3 py-2 text-left hover:text-orange'>Vietnamese</button>
-                  <button className='mt-2 px-3 py-2 text-left hover:text-orange'>English</button>
-                </div>
-              </div>
-            }
-          >
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              strokeWidth={1.5}
-              stroke='currentColor'
-              className='h-5 w-5'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                d='M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418'
-              />
-            </svg>
-            <span className='ml-1'>Vietnamese</span>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              strokeWidth={1.5}
-              stroke='currentColor'
-              className='h-5 w-5'
-            >
-              <path strokeLinecap='round' strokeLinejoin='round' d='M19.5 8.25l-7.5 7.5-7.5-7.5' />
-            </svg>
-          </Popover>
-          <Popover
-            className='flex items-center py-1 hover:text-gray-300'
-            renderPopover={
-              <div>
-                <Link
-                  to={path.profile}
-                  className='block w-full bg-white px-3 py-3 text-left hover:bg-slate-100 hover:text-cyan-500'
-                >
-                  Tài khoản của tôi
-                </Link>
-                <Link
-                  to={path.purchase}
-                  className='block w-full bg-white px-3 py-3 text-left hover:bg-slate-100 hover:text-cyan-500'
-                >
-                  Đơn mua
-                </Link>
-                <button
-                  onClick={onLogOut}
-                  className='block w-full px-3 py-3 text-left hover:bg-slate-100 hover:text-cyan-500'
-                >
-                  Đăng xuất
-                </button>
-              </div>
-            }
-          >
-            <div className='h-5 w-5 flex-shrink-0'>
-              <img
-                src='https://down-vn.img.susercontent.com/file/8fcc324015ee3e14e8c8b1e88fe7a548_tn'
-                alt='avatar'
-                className='h-full w-full rounded-full object-cover'
-              />
-            </div>
-            <div className=' pl-2 hover:cursor-pointer'>{profile?.email}</div>
-          </Popover>
-        </div>
+        <NavHeader />
 
         <div className='mt-4 grid grid-cols-12 items-end gap-4'>
           <Link to='/' className='col-span-2'>
