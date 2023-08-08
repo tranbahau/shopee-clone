694901692e6e7ecb9ad7b040c7353aa57d394615
Trần Bá Hậu <bahau2411.work@gmail.com>
2023-08-08 19:52:17 +0700

    [ShopeeClone]_Update phone validator

--- src/utils/rules.ts
+++ src/utils/rules.ts
@@ -100,7 +100,7 @@ export const schema = yub.object({
 
 export const userSchema = yub.object({
   name: yub.string().max(160, 'Độ dài tối đa là 160 kí tự'),
-  phone: yub.string(),
+  phone: yub.string().max(20, 'Độ dài tối đa là 20 kí tự'),
   address: yub.string().max(160, 'Độ dài tối đa là 160 kí tự'),
   date_of_birth: yub.date().max(new Date(), 'Hãy chọn một ngày trong quá khứ'),
   password: schema.fields['password'],
